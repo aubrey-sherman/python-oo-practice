aeef7c3bab80d25bc94e9169be6993a1e1f04478
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-04-23 17:56:58 -0400

    Filled out functionality for SerialGenerator class methods.

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,17 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    def __init__(self, start):
+        """ Create a serial generator that begins at start (number). """
+        self.start = start - 1
+        self.current = start - 1
+
+    def generate(self):
+        """ Given the current count, return next sequential number. """
+        self.current += 1
+        return self.current
+
+    def reset(self):
+        """ Reset current count to original start number. """
+        self.current = self.start
