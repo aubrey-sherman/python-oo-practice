1ee8f15e11ea0e4e26213a6c628d99a300b9694d
Andrea Jao <ndrjao@gmail.com>
2024-04-23 15:54:55 -0700

    created and tested the init function

--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,30 @@
+def read_file(file_path):
+    """take in a file path (aboslute or relative) and will
+        return an object representing the file
+    """
+
+    file_to_be_read = open(f"{file_path}", "r")
+    return file_to_be_read
+
+
+def count_words_read(file):
+    """take in a file object, will print the number of the words read
+    """
+    words_count = len(file.readlines())
+    print(f"{words_count} words read")
+
+
 class WordFinder:
-    """Word Finder: finds random words from a dictionary."""
+    """Word Finder: finds random words from a dictionary.
+    """
+
+    def __init__(self, file_path):
+        """ Will create an object representing the file to be read and a
+            count property for the number of words read
+        """
+        self.file_object = read_file(file_path)
+        # double check it to pass in file_path as param
+        self.words_read_count = count_words_read(self.file_object)
+
+    def random(list_of_words):
+        """Given a list of words, will return a random word"""
