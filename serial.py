c77b817ca443e243540dd3a3bf31e445bd206aed
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-04-23 19:47:33 -0400

    Wrote .random method for Wordfinder class; updated init function so file does not need to be re-read.

--- serial.py
+++ serial.py
@@ -21,13 +21,13 @@ class SerialGenerator:
     def __init__(self, start):
         """ Create a serial generator that begins at start (number). """
         self.start = start - 1
-        self.current = start - 1
+        self.previous_value = start - 1
 
     def generate(self):
         """ Given the current count, return next sequential number. """
-        self.current += 1
-        return self.current
+        self.previous_value += 1
+        return self.previous_value
 
     def reset(self):
         """ Reset current count to original start number. """
-        self.current = self.start
+        self.previous_value = self.start
--- wordfinder.py
+++ wordfinder.py
@@ -1,3 +1,6 @@
+from random import choice as pick_a_word
+
+
 def read_file(file_path):
     """take in a file path (aboslute or relative) and will
         return an object representing the file
@@ -7,10 +10,10 @@ def read_file(file_path):
     return file_to_be_read
 
 
-def count_words_read(file):
+def count_words_read(word_list):
     """take in a file object, will print the number of the words read
     """
-    words_count = len(file.readlines())
+    words_count = len(word_list)
     print(f"{words_count} words read")
 
 
@@ -22,9 +25,12 @@ class WordFinder:
         """ Will create an object representing the file to be read and a
             count property for the number of words read
         """
-        self.file_object = read_file(file_path)
+        self.word_list = read_file(file_path).readlines()
         # double check it to pass in file_path as param
-        self.words_read_count = count_words_read(self.file_object)
+        self.words_read_count = count_words_read(self.word_list)
 
-    def random(list_of_words):
+    def random(self):
         """Given a list of words, will return a random word"""
+        list_of_words = self.word_list
+        random_word = pick_a_word(list_of_words)
+        return random_word.replace('\n', "")
