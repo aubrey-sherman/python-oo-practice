c4f35547c16d0a2e928e324a9a4ded0537c6f231
Andrea Jao <ndrjao@gmail.com>
2024-04-23 18:01:21 -0700

    completed functions

--- wordfinder.py
+++ wordfinder.py
@@ -1,22 +1,6 @@
 from random import choice as pick_a_word
 
 
-def read_file(file_path):
-    """take in a file path (aboslute or relative) and will
-        return an object representing the file
-    """
-
-    file_to_be_read = open(f"{file_path}", "r")
-    return file_to_be_read
-
-
-def count_words_read(word_list):
-    """take in a file object, will print the number of the words read
-    """
-    words_count = len(word_list)
-    print(f"{words_count} words read")
-
-
 class WordFinder:
     """Word Finder: finds random words from a dictionary.
     """
@@ -25,12 +9,44 @@ class WordFinder:
         """ Will create an object representing the file to be read and a
             count property for the number of words read
         """
-        self.word_list = read_file(file_path).readlines()
-        # double check it to pass in file_path as param
-        self.words_read_count = count_words_read(self.word_list)
+        self.formatted_word_list = self.read_and_format(file_path)
+        self.words_read_count = self.count_words_read(self.formatted_word_list)
+
+    def read_and_format(self, file_path):
+        """take in a file path (aboslute or relative) and will
+            return an object representing the file
+        """
+        word_list = open(f"{file_path}", "r").readlines()
+        formatted_list = [item.strip() for item in word_list]
+
+        return formatted_list
+
+    def count_words_read(self, word_list):
+        """take in a file object, will print the number of the words read
+        """
+        words_count = len(word_list)
+        print(f"{words_count} words read")
 
     def random(self):
         """Given a list of words, will return a random word"""
-        list_of_words = self.word_list
+        list_of_words = self.formatted_word_list
         random_word = pick_a_word(list_of_words)
-        return random_word.replace('\n', "")
+        return random_word
+
+
+class SpecialWordFinder(WordFinder):
+    """Special Word Finder: find a random word in a document with special
+        characters
+    """
+
+    def __init__(self, file_path):
+        """ Will create an object representing the file to be read and a
+            count property for the number of words read
+        """
+        super().__init__(file_path)
+        # print(self.formatted_word_list)
+        self.formatted_word_list = [item for item in
+                                    [
+                                        item for item in self.formatted_word_list
+                                        if item != ""] if item[0] != "#"
+                                    ]
